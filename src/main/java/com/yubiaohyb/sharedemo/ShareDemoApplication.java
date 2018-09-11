07bdbc5eed2a5e5bd83bae82a7926e6fa3634527
yubiaohyb <971449932@qq.com>
2018-09-11 23:46:56 +0800

    新增：初始化项目
    Signed-off-by: yubiaohyb <971449932@qq.com>

---
+++ src/main/java/com/yubiaohyb/sharedemo/ShareDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yubiaohyb.sharedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareDemoApplication.class, args);
+    }
+}
