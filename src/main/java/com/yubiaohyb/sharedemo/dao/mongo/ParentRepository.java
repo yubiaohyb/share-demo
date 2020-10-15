0e77295a4613475f97e2371e4a70a536e48c209c
yubiaohyb <971449932@qq.com>
2020-10-16 02:17:33 +0800

    添加springboot学习调整

--- src/main/java/com/yubiaohyb/sharedemo/dao/mongo/ParentRepository.java
+++ src/main/java/com/yubiaohyb/sharedemo/dao/mongo/ParentRepository.java
@@ -1,17 +1,17 @@
-package com.yubiaohyb.sharedemo.dao.mongo;
-
-import com.yubiaohyb.sharedemo.model.ParentDo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-/**
- * 人若志趣不远，心不在焉，虽学不成。
- * <p>
- * description  -  functionDescrption
- *
- * @author Yubiao Huang (https://github.com/yubiaohyb)
- * @version $$Id$$
- * @since 2019-05-25 21:08
- */
-public interface ParentRepository extends MongoRepository<ParentDo, String> {
-
-}
+//package com.yubiaohyb.sharedemo.dao.mongo;
+//
+//import com.yubiaohyb.sharedemo.model.ParentDo;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+///**
+// * 人若志趣不远，心不在焉，虽学不成。
+// * <p>
+// * description  -  functionDescrption
+// *
+// * @author Yubiao Huang (https://github.com/yubiaohyb)
+// * @version $$Id$$
+// * @since 2019-05-25 21:08
+// */
+//public interface ParentRepository extends MongoRepository<ParentDo, String> {
+//
+//}
--- src/main/java/com/yubiaohyb/sharedemo/model/ParentDo.java
+++ src/main/java/com/yubiaohyb/sharedemo/model/ParentDo.java
@@ -1,7 +1,7 @@
 package com.yubiaohyb.sharedemo.model;
 
 import lombok.Data;
-import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * 人若志趣不远，心不在焉，虽学不成。
@@ -12,7 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * @version $$Id$$
  * @since 2019-05-25 20:58
  */
-@Document(collection = "parents")
+//@Document(collection = "parents")
 @Data
 public class ParentDo {
 
